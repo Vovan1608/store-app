c9cd322f46c305ac32d8e4748d51a78d98b86261
Vladymyr Zenin <vovan1608@gmail.com>
2022-07-18 15:28:16 +0300

    add store

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,25 @@
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import ErrorBoundary from '../errorBoundary';
+import BookstoreService from '../../services/BookstoreService';
+import { BookstoreServiceProvider } from '../bookstoreServiceContext';
+
+import store from '../../store';
+
+const bookstoreService = new BookstoreService();
 
 const App = () => {
     return (
-        <ErrorBoundary error={false}>
-            <div>
-                Hello
-            </div>
-        </ErrorBoundary>
+        <Provider store={store}>
+            <ErrorBoundary error={false}>
+                <BookstoreServiceProvider value={bookstoreService}>
+                    <Router>
+                        <h1>Hello</h1>
+                    </Router>
+                </BookstoreServiceProvider>
+            </ErrorBoundary>
+        </Provider>
     );
 }
 
