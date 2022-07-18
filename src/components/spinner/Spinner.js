a235cb588642273a8ec568e71a2e1c74c00222fd
Vladymyr Zenin <vovan1608@gmail.com>
2022-07-18 12:54:37 +0300

    add base structure

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,14 @@
+import './Spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-lrmv6yt75n"><div className="ldio-qazsl2ptjn">
+            <div></div>
+            <div></div>
+            <div><div></div></div>
+            <div><div></div></div>
+        </div></div>
+    );
+}
+
+export default Spinner;
