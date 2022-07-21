c929b4bb4b923b0724e7188ea7e4dfb9ebff4a43
Vladymyr Zenin <vovan1608@gmail.com>
2022-07-21 11:45:37 +0300

    fix header

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,26 @@
+import { connect } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
 import Header from '../header';
 import { Cart, Home } from '../pages';
 
-const App = () => {
+const App = ({ cartItems = [], orderTotal = 0 }) => {
     return (
         <main role='main' className='container'>
-            <Header numItems={5} total={210} />
+            <Header numItems={cartItems.length} total={orderTotal} />
             <Routes>
                 <Route path='/' element={<Home />} exact />
                 <Route path='/cart' element={<Cart />} />
             </Routes>
         </main>
     );
-}
+};
 
-export default App;
+const mapStateToProps = ({ shoppingCart: { cartItems, orderTotal } }) => {
+    return {
+        cartItems,
+        orderTotal
+    };
+};
+
+export default connect(mapStateToProps)(App);
